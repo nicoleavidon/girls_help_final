538ac7ef1c2bd4e11b2818ec688bccf6fdd8f207
nicoleavidon <nicoleavidon@gmail.com>
2017-08-11 11:27:02 -0700

    fixed home page

--- blog/urls.py
+++ blog/urls.py
@@ -6,9 +6,9 @@ from django.conf import settings
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-url(r'^home/', views.home, name='home'),
+url(r'^home/', views.post_list, name='post_list'),
 url(r'^login/$', auth_views.login),
-url(r'^$', views.post_list, name='post_list'),
+url(r'^$', views.home, name='home'),
 url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 url(r'^post/new/$', views.post_new, name='post_new'),
 url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
